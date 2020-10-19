26ec66310e21610a1463048a21c38934b22f9b2c
Charlie Pelton <charliejp0311@gmail.com>
2020-10-19 11:08:04 -0500

    Done.

--- .history/src/components/todos/CreateTodo_20201019103403.js
+++ .history/src/components/todos/CreateTodo_20201019103403.js
@@ -13,7 +13,6 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
-    this.setState({text: '',})
   }
 
   handleChange(event) {
---
+++ .history/src/components/todos/TodosContainer_20201019104028.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class TodosContainer extends Component {
+
+    render(){
+        return(
+            <idv></idv>
+        );
+    }
+};
+
+export default connect()(TodosContainer);
